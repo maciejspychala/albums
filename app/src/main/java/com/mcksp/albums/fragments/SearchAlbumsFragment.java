f3967efead4f70d4c4610f8b7b0e56578d5ddb6f
maciejspychala <maciej.spychala@bitgamelabs.com>
2019-06-10 23:11:42 +0200

    add recyclerview

--- app/src/main/java/com/mcksp/albums/fragments/SearchAlbumsFragment.java
+++ app/src/main/java/com/mcksp/albums/fragments/SearchAlbumsFragment.java
@@ -4,20 +4,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.mcksp.albums.R;
 import com.mcksp.albums.adapter.AlbumsAdapter;
 import com.mcksp.albums.models.Album;
 
+import java.util.ArrayList;
+
 public class SearchAlbumsFragment extends Fragment implements AlbumsAdapter.OnAlbumClick {
 
     public static final String TAG = SearchAlbumsFragment.class.getSimpleName();
     private static final String ALBUM_KEY = "ALBUM_KEY";
+    RecyclerView recyclerView;
+    AlbumsAdapter adapter;
+    ArrayList<Album> albums = new ArrayList<>();
 
     private Album albumToChange;
 
@@ -41,12 +47,23 @@ public class SearchAlbumsFragment extends Fragment implements AlbumsAdapter.OnAl
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView title = view.findViewById(R.id.search_title);
-        title.setText(albumToChange.title);
+        EditText title = view.findViewById(R.id.search_text);
+        title.setText(formatString(albumToChange.title));
+        adapter = new AlbumsAdapter(albums, this);
+        recyclerView = view.findViewById(R.id.album_search_list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
     }
 
     @Override
     public void onAlbumClicked(Album album) {
         Log.d("SEARCH", album.title);
+    }
+
+    private static String formatString(String str) {
+        str = str.replaceAll("\\(.*?\\) ?", "");
+        str = str.replaceAll("\\[.*?\\] ?", "");
+        str = str.replaceAll("\\<.*?\\> ?", "");
+        return str.trim();
     }
 }
